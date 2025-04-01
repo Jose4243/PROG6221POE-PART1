55fddf1e1fb407c288b111edcad7813cb8168952
Jose4243 <josecontreiras14@gmail.com>
2025-04-01 12:54:42 +0200

    ADD-Greeting
    Added a greeting and user interaction functionality with a voice recording from Eleven labs and an ASCII image

--- PROG6221-PART1/Program.cs
+++ PROG6221-PART1/Program.cs
@@ -7,6 +7,64 @@ namespace CybersecurityAiChatBot
 {
     class Program
     {
+        
+            DisplayImage();
+            PLayVoiceGreeting();
+            
+            
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("\nEnter Name: " ,60);
+            string userName = Console.ReadLine();
+            if (userName.Equals(""))
+            {
+                Console.WriteLine("Please enter a name!");
+                
+            }
+            else
+            {
+
+                Print("\nWelcome " + userName + "!");
+            }
+
+            UserInteraction(userName);
+
+
+
+
+            static void PLayVoiceGreeting()
+            {
+
+
+                var myPlayer = new SoundPlayer();
+                myPlayer.SoundLocation = @"C:\Users\contr\source\repos\TestCode\TestCode\ElevenLabs_2025-03-26T15_56_04_George_pre_s50_sb75_se57_b_m2.wav";
+                myPlayer.PlaySync();
+
+            }
+
+
+
+            static void DisplayImage()
+            {
+
+                string logo = "      ───▄█▌─▄─▄─▐█▄\r\n" +
+                    "      ───██▌▀▀▄▀▀▐██\r\n" +
+                    "      ───██▌─▄▄▄─▐██\r\n" +
+               "      ───▀██▌▐█▌▐██▀\r\n" +
+                    "      ▄██████─▀─██████▄\r\n";
+
+                    
+                  
+
+
+
+                Console.WriteLine(logo);
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine("THE CYBERSECURITY AWARENESS BOT");
+               
+            }
+
+
+
 
 
         static void Main(string[] args)
